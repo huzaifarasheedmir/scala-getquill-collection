105f7eb22be8d3a7eec3870b36488b8e2bc9aefc
huzaifarasheedmir <huzaifa@wanclouds.net>
2017-02-22 16:28:09 -0500

    Add encoder/decoder for Map and nested Collections

--- src/main/scala/collection/Main.scala
+++ src/main/scala/collection/Main.scala
@@ -1,10 +1,13 @@
 package collection
 
+import com.google.common.reflect.TypeToken
 import io.getquill.{CassandraAsyncContext, SnakeCase}
 import io.getquill.context.cassandra.CassandraSessionContext
 
-import scala.reflect.ClassTag
 import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.reflect.ClassTag
+
 import scala.util.{Failure, Success}
 
 
@@ -12,8 +15,30 @@ import scala.util.{Failure, Success}
 trait Encoders {
   this: CassandraSessionContext[_] =>
 
+  /*
+  Will work List with any non collection data type
+   */
   implicit def listEncoder[T](implicit t: ClassTag[T]): Encoder[List[T]] =
     encoder((index, value, row) => row.setList(index, value.asJava, t.runtimeClass.asInstanceOf[Class[T]]))
+
+
+  /*
+  Will work for list of Map[String, String]
+   */
+  val token = new TypeToken[java.util.Map[String, String]]() {}
+  implicit val listMapEncoder: Encoder[List[java.util.Map[String, String]]] =
+    encoder((index, value, row) => row.setList(index, value.asJava, token))
+
+
+  /*
+  Will work for Map[String, Sting] both key and value should be of same data types or explicitly
+  specify the types
+   */
+
+  implicit def mapEncoder[T](implicit t: ClassTag[T]): Encoder[Map[T, T]] =
+    encoder((index, value, row) => row.setMap(index, value.asJava))
+
+
 }
 
 trait Decoders {
@@ -21,6 +46,14 @@ trait Decoders {
 
   implicit def listDecoder[T](implicit t: ClassTag[T]): Decoder[List[T]] =
     decoder((index, row) => row.getList(index, t.runtimeClass.asInstanceOf[Class[T]]).asScala.toList)
+
+  val token = new TypeToken[java.util.Map[String, String]]() {}
+  implicit val listMapDecoder: Decoder[List[java.util.Map[String, String]]] =
+    decoder((index, row) => row.getList(index, token).asScala.toList)
+
+  implicit def mapDecoder[T](implicit t: ClassTag[T]): Decoder[Map[T, T]] =
+    decoder((index, row) => row.getMap(index, t.runtimeClass.asInstanceOf[Class[T]], t.runtimeClass.asInstanceOf[Class[T]]).asScala.toMap)
+
 }
 
 object Main extends App{
@@ -30,6 +63,9 @@ object Main extends App{
 
   val entry = new Fruits("2", List("Mango", "Kiwi"))
 
+  val mixEntry = new Mix("1", List("a", "b"),Map("A" -> "a"), List(Map("A" -> "a").asJava, Map("B" -> "b").asJava))
+
+
   implicit val movieSchemaMeta = schemaMeta[Fruits]("fruits", _.id -> "id", _.fruit_list-> "fruit_list")
 
   val insertFruits = quote {
@@ -37,12 +73,23 @@ object Main extends App{
   }
   context.run(insertFruits)
 
+  val insertMix = quote {
+    query[Mix].insert(lift(mixEntry)).ifNotExists
+  }
+  context.run(insertFruits)
+
   val getFruits = quote {
     query[Fruits].filter(row => row.id=="1")
-
+  }
+  context.run(getFruits).onComplete{
+    case Success(lst) => println(lst)
+    case Failure(exception)=> println(exception.getMessage)
   }
 
-  context.run(getFruits).onComplete{
+  val getMix = quote {
+    query[Mix].take(10)
+  }
+  context.run(getMix).onComplete{
     case Success(lst) => println(lst)
     case Failure(exception)=> println(exception.getMessage)
   }
--- src/main/scala/collection/models.scala
+++ src/main/scala/collection/models.scala
@@ -1,3 +1,5 @@
 package collection
 
 case class Fruits(id: String, fruit_list: List[String])
+
+class Mix(id: String, ls : List[String], mp: Map[String, String], lsmp: List[java.util.Map[String, String]])
